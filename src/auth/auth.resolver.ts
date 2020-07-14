1e5390f625eb28c8e5355a664f39e4982db7f4e4
Shoaib Sharif <sharifshoaib@yahoo.com>
2020-07-13 20:07:34 -0700

    all about headersheaders

--- src/auth/auth.resolver.ts
+++ src/auth/auth.resolver.ts
@@ -62,7 +62,7 @@ export class AuthResolver {
       httpOnly: true,
       secure: process.env.NODE_ENV !== 'development',
       maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-      sameSite: false,
+      sameSite: 'none',
     });
     return session;
   }
