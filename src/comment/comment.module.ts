6ecc33f43f181b759520f76525d58504b665e7cb
King Rayhan <rayhan095@gmail.com>
2020-07-29 04:32:00 +0600

    comment count

--- src/comment/comment.module.ts
+++ src/comment/comment.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { CommentService } from './comment.service';
 import { TypegooseModule } from 'nestjs-typegoose';
 import { Comment } from './comment.type';
@@ -6,7 +6,11 @@ import { ArticleModule } from 'src/article/article.module';
 import { CommentResolver } from './comment.resolver';
 
 @Module({
-  imports: [TypegooseModule.forFeature([Comment]), ArticleModule],
+  imports: [
+    TypegooseModule.forFeature([Comment]),
+    forwardRef(() => ArticleModule),
+  ],
   providers: [CommentService, CommentResolver],
+  exports: [CommentService],
 })
 export class CommentModule {}
